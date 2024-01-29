7142571912b272c58a5401dd8d4c66d72e1f4d01
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-01-29 17:57:59 +0900

    pagination 코드 최적화

--- src/main/java/com/farm/controller/FarmController.java
+++ src/main/java/com/farm/controller/FarmController.java
@@ -20,32 +20,28 @@ public class FarmController {
     public String list(@RequestParam(value = "page", defaultValue = "1") int page, Model model){
         // 페이지 관련 처리
         int nowPage = page - 1;
-        Pageable pageable = PageRequest.of(nowPage, 10);
+        int pageSize = 10;
+        int pagePerBlock = 5;
+        Pageable pageable = PageRequest.of(nowPage, pageSize);
         Page<Farm> result = listService.findAll(pageable);
 
         // 페이지네이션 관련 변수 계산
-        int pagePerblock = 5;
-        Long totalElements = result.getTotalElements(); // 전체 레코드 수
         int totalPages = result.getTotalPages(); // 전체 페이지 수
-        int size = result.getSize(); // 페이지당 아이템 수
         int pageNumber = result.getNumber() + 1; // 현재 페이지 번호
 
         // 페이지네이션 시작 및 끝 페이지 계산
-        int startPage = ((page - 1) / pagePerblock) * pagePerblock + 1;
-        int endPage = startPage + pagePerblock - 1;
+        int startPage = ((page - 1) / pagePerBlock) * pagePerBlock + 1;
+        int endPage = Math.min(startPage + pagePerBlock - 1, totalPages);
 
         // 뷰에 데이터 전달
         model.addAttribute("nowPage", nowPage);
-        model.addAttribute("totalElements", totalElements);
         model.addAttribute("totalPages", totalPages);
-        model.addAttribute("size", size);
         model.addAttribute("pageNumber", pageNumber);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("farms", result.getContent());
         return "list";
     }
-
 
 }
 
