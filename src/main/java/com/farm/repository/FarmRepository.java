5ba6ed60de6b04d69d5e2a572328f0e600f0b603
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-01-30 16:06:25 +0900

    검색 기능 구현

--- src/main/java/com/farm/repository/FarmRepository.java
+++ src/main/java/com/farm/repository/FarmRepository.java
@@ -1,10 +1,17 @@
 package com.farm.repository;
 
 import com.farm.domain.Farm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FarmRepository extends JpaRepository<Farm, Long> {
 
+    Page<Farm> findByWfAddrContaining(String keyword, Pageable pageable);
+
+    Page<Farm> findByWfSubjectContaining(String keyword, Pageable pageable);
+
+    Page<Farm> findByWfThemeContaining(String keyword, Pageable pageable);
 }
